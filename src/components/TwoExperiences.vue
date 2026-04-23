afbc58dd238c3116dc70503254639817a9bf8382
luuu6 <lucianamechan@gmail.com>
2026-04-23 13:50:06 -0500

    feat(two-exp): add two experiences section structure and content

---
+++ src/components/TwoExperiences.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="two-experiences" id="two-experiences">
+    <div class="container">
+
+      <!-- TITLE -->
+      <h2 class="title">Two experiences</h2>
+
+      <!-- CARD 1 -->
+      <div class="experience-card">
+        <h3>¿Eres un cliente?</h3>
+        <p class="subtitle">
+          ¿Sientes que no recibes respuestas de las ópticas?, ¿retrasos largos y falta de comunicación?
+        </p>
+        <p>
+          OptiFlow permite que puedas rastrear desde una aplicación web el estado de tu orden,
+          sin tener que ir al local físicamente o tener que insistir constantemente por información a la empresa.
+        </p>
+      </div>
+
+      <!-- CARD 2 -->
+      <div class="experience-card">
+        <h3>¿Eres una óptica?</h3>
+        <p>
+          Mejora el CRM de tu empresa mediante la aplicación para clientes, mejora la emisión y el rastreo de órdenes y citas,
+          mejora el registro, creación y análisis de ventas y organiza la información de tus clientes.
+        </p>
+        <p>
+          Nuestro sistema permitirá acelerar tu flujo de trabajo, minimizando el margen de error al digitalizar
+          y concentrar toda la comunicación en un solo sistema práctico y sencillo de usar.
+        </p>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
