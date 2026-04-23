e1a8a19d0d160249f40427699116d0cc87b418f9
Fernando N. <nicholasbeckenbauer@gmail.com>
2026-04-23 18:21:54 -0500

    Update Footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import FeaturesSection from './components/FeaturesSection.vue'
 import NavbarComponent from "./components/NavbarComponent.vue";
 import HeroCopmonent from "./components/HeroComponent.vue";
 import ContactSection from "./components/ContactSection.vue";
+import Footer from "./components/Footer.vue";
 
 </script>
 <template>
@@ -10,4 +11,5 @@ import ContactSection from "./components/ContactSection.vue";
   <HeroCopmonent/>
   <FeaturesSection />
   <ContactSection />
+  <Footer />
 </template>
